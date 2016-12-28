07f161ed4ed8a39d8a90d92e20dcfe19918566c3
mayur.gupta <mayur.gupta@tothenew.com>
2016-12-28 19:27:24 +0530

    New code for last question

--- src/App.js
+++ src/App.js
@@ -2,92 +2,139 @@
 // Form should have a submit button. Apply form validation to incorporate following rules.
 
 
-import React from 'react';
+import React from 'react'
+
+
 export default class App extends React.Component {
   render() {
     return (
-      <Registration/>
+        <UserRegisration/>
     );
   }
 }
-class Registration extends React.Component {
-  constructor() {
-    super();
+
+class UserRegisration extends React.Component {
+
+  constructor(props){
+    super(props)
     this.state = {
-      text: {
-        FirstName:"",
-        LastName:"",
-        EmailId:"",
-        Contact:"",
-        Password:"",
-        ConfirmPassword:""
-      },validations: {
-        isFirstNameValid:true,
-        isFirstNameLastName:true,
-        isLastNameValid:true,
-        isEmailIdValid:true,
-        isContactValid:true,
-        isPasswordValid:true
-      }
-    };
+      firstNameTxt : "",
+      lastNameTxt : "",
+      emailTxt : "",
+      contactNumberTxt : "",
+      passwdTxt : ""
+    }
 
-    this.setInput = this.setInput.bind(this);
+    this.submitForm = this.submitForm.bind(this)
+
   }
 
-  setInput(e){
-    let initialState=this.state.text;
-    initialState[e.target.name]=e.target.value;
-    this.setState(initialState);
-  }
-
-
-  render() {
-    return (
-        <div>
-
-          FirstName:  <InputEle type="text" value ={this.state.text.FirstName} name ="FirstName"  onChange = {this.setInput} />
-          <div>
-            {this.state.validations.isFirstNameValid} ?  <span></span> : <span>Please enter First Name</span>
-          </div>
-          <div>
-            {this.state.validations.isFirstNameLastName} ?  <span></span> : <span>First Name should not be equal to LastName</span>
-          </div>
-
-
-          LastName: <InputEle type="text" value ={this.state.text.LastName}  name ="LastName" onChange = {this.setInput} />
-          <div>
-            {this.state.validations.isLastNameValid} ?  <span></span> : <span>Please enter Last Name</span>
-          </div>
-
-          EmailId: <InputEle type="text" value ={this.state.text.EmailId} name ="EmailId"  onChange = {this.setInput} />
-          <div>
-            {this.state.validations.isEmailIdValid} ?  <span></span> : <span>Please enter Valid Email ID</span>
-          </div>
-
-          Contact: <InputEle type="text" value ={this.state.text.Contact}  name ="Contact" onChange = {this.setInput} />
-
-          <div>
-            {this.state.validations.isContactValid} ?  <span></span> : <span>Contact number should be of 10 digit numbers only.</span>
-          </div>
-          Password: <InputEle type="password" value ={this.state.text.Password} name ="Password"  onChange = {this.setInput} />
-          Confirm Password: <InputEle type="password" value ={this.state.text.ConfirmPassword} name ="ConfirmPassword"  onChange = {this.setInput} />
-
-          <div>
-            {this.state.validations.isPasswordValid} ?  <span></span> : <span>Password and Confirm Password should be same.</span>
-          </div>
-        </div>
-    );
-  }
+  validateEmail(value) {
+  // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+  let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(value);
 }
+  submitForm(){
 
-class InputEle extends React.Component{
-  render() {
-    return (
-      <div>
-        <input type = {this.props.type} name={this.props.name} value = {this.props.value} onChange = {this.props.onChange} />
-      </div>
-    );
+    if(this.refs.firstNameRef.length){
+
+      if(this.refs.firstNameRef.value === this.refs.lastNameRef.value){
+        this.setState({
+          firstNameTxt : "First Name should not be equal to LastName",
+          lastNameTxt : "First Name should not be equal to LastName"
+        })
+      }else{
+        this.setState({
+          firstNameTxt : "Validation Passed."
+        })
+      }
+    } else {
+      this.setState({
+        firstNameRef : "Please enter FirstName."
+      })
+    }
+
+    if(this.refs.lastNameRef.length){
+
+      if(this.refs.firstNameRef.value === this.refs.lastNameRef.value){
+        this.setState({
+          firstNameTxt : "First Name should not be equal to LastName",
+          lastNameTxt : "First Name should not be equal to LastName"
+        })
+      }else{
+        this.setState({
+          lastNameTxt : "Validation Passed."
+        })
+      }
+    } else {
+      this.setState({
+        lastNameRef : "Please enter FirstName."
+      })
+    }
+
+    if(this.refs.passwdRef.value === this.refs.confPasswdRef.value){
+      this.setState({
+        passwdTxt : "Validation Passed."
+      })
+    } else {
+      this.setState({
+        passwdTxt : "Password and Confirm password should be same."
+      })
+    }
+
+    let contactNo = this.refs.contactNumberRef.value
+
+    if(contactNo.length === 10){
+      this.setState({
+        contactNumberTxt : "Validation Passed."
+      })
+    } else {
+      this.setState({
+        contactNumberTxt : "Contact number should be of 10 digit numbers only."
+      })
+    }
+
+    let emailId = this.refs.emailRef.value
+
+    if(this.validateEmail(emailId)){
+      this.setState({
+        contactNumberTxt : "Validation Passed."
+      })
+    } else {
+      this.setState({
+        contactNumberTxt : "Email should be a valid email."
+      })
+    }
+
   }
+
+  render(){
+
+    return(
+      <div>
+        First Name : <input ref="firstNameRef" type="text" name="firstName" />
+        <div id="firstNameDiv">{this.state.firstNameTxt}</div>
+
+        Last Name : <input ref="lastNameRef" type="text" name="lastName" />
+        <div id="lastNameDiv">{this.state.lastNameTxt}</div>
+
+        Email : <input ref="emailRef" type="text" name="email" />
+        <div id="emailDiv">{this.state.emailTxt}</div>
+
+        Contact Number : <input ref="contactNumberRef" type="text" name="contactNumber" />
+        <div id="contactNumberDiv">{this.state.contactNumberTxt}</div>
+
+        Password : <input ref="passwdRef" type="text" name="password" />
+        <div id="password1">{this.state.passwdTxt}</div>
+        Confirm Password : <input ref="confPasswdRef" type="text" name="confirmPassword" />
+        <div id="password2">{this.state.passwdTxt}</div>
+
+        <button onClick={this.submitForm}>Submit</button>
+      </div>
+    )
+
+  }
+
 }
 
 
